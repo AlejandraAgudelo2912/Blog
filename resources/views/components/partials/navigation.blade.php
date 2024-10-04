bc1b9a400e5194aa9d8a182d96da8c80ddffa667
LAPTOP-1DQNJ8L3\aleja <3369210@alu.murciaeduca.es>
2024-10-04 16:52:15 +0200

    paso 5

---
+++ resources/views/components/partials/navigation.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="{{route('Home')}}">Home</a></li>
+    <li><a href="{{route('contact')}}">Contacto</a></li>
+    <li><a href="{{route('blog')}}">Blog</a></li>
+    <li><a href="{{route('about')}}">Nosotros</a></li>
+</ul>
