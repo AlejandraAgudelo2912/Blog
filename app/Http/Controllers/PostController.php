8ab209a4fbe592fca592b183d46b7a9207000766
LAPTOP-1DQNJ8L3\aleja <3369210@alu.murciaeduca.es>
2024-10-11 16:17:08 +0200

    Paso 14

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,6 +38,11 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|min:5',
+            'body' => 'required',
+        ]);
+
         $post = new Post();
         $post->title=$request->input('title');
         $post->body=$request->input('body');
