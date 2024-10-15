d41b161e7b042499bb2d6eb2a73c8d5097d7d290
LAPTOP-1DQNJ8L3\aleja <3369210@alu.murciaeduca.es>
2024-10-15 17:11:13 +0200

    Fin paso 20

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -66,4 +66,10 @@ class PostController extends Controller
 
         return redirect()->route('posts.show', $post)->with('status', 'El post se ha actualizado correctamente');
     }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index')->with('status', 'El post se ha eliminado correctamente');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,18 @@ use Illuminate\Support\Facades\Route;
 Route::view(uri:'/',view:'welcome')->name('Home') ;
 
 Route::view(uri:'contacto',view:'contact')->name('contact');
-Route::get('blog', [PostController::class, 'index'])->name('posts.index');//entre corchetes para decirle que metodo hay que llamar
+
+Route::resource('blog',PostController::class)//solo para las siete rutas estandars
+    ->names('posts')
+    ->parameters(['blog' => 'post'])//que mierda es esto
+;
+/*Route::get('blog', [PostController::class, 'index'])->name('posts.index');//entre corchetes para decirle que metodo hay que llamar
 Route::get('blog/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('blog', [PostController::class, 'store'])->name('posts.store');
 Route::get('blog/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('blog/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::patch('blog/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('blog/{post}', [PostController::class, 'destroy'])->name('posts.destroy');*/
 Route::view(uri:'nosotros',view:'about')->name('about');
 
 /*Route::match(['put', 'patch'], uri:'/', function(){
