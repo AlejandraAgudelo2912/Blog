6aeb07a1f524c997e613e0e4e3564571f614b08b
LAPTOP-1DQNJ8L3\aleja <3369210@alu.murciaeduca.es>
2024-10-18 17:10:39 +0200

    Paso 28 a medias

--- resources/views/posts/form-fields.blade.php
+++ resources/views/posts/form-fields.blade.php
@@ -4,13 +4,13 @@
                   name="title"
                   type="text"
                   value="{{old('title',$post->title)}}"
-                  class="w-full mt-1"
+                  class="block w-full mt-1"
     />
-    <x-input-error :messages="$errors->get('title')"></x-input-error>
+    <x-input-error :messages="$errors->get('title')" class="mt-2"></x-input-error>
 </div>
 <div>
-    <x-input-label for="body" :value="__('Body')" ></x-input-label>
-    <x-textarea id="body" name="body" class="w-full mt-1">{{old('body',$post->body)}}</x-textarea>
+    <x-input-label for="body" :value="__('Body')" class="mt-2"></x-input-label>
+    <x-textarea id="body" name="body" class="block w-full mt-1">{{old('body',$post->body)}}</x-textarea>
     <x-input-error :messages="$errors->get('body')"></x-input-error>
 
 </div>
