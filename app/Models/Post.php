5e87f5935c2b2eb2f81453b1160e88ae4ce9f021
LAPTOP-1DQNJ8L3\aleja <3369210@alu.murciaeduca.es>
2024-11-06 15:59:40 +0100

    Ejercicio 3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'body'];
     //protected $table = 'articles';//si le camniamos el nombre en la base de datos a posts a articles con nombrarla aqi sobra.
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
